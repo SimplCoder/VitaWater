ea9b65c69ea6d0444a91a4791a566e350f79c41d
Rupesh Dhadiwal <dhadiwalrupesh7@gmail.com>
2022-03-18 16:20:13 +0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDs3q-i7tsLtpy0vUJJ6Ejbk6LxeTQ6GiA",
-  authDomain: "vitawater-9b375.firebaseapp.com",
-  projectId: "vitawater-9b375",
-  storageBucket: "vitawater-9b375.appspot.com",
-  messagingSenderId: "395378570353",
-  appId: "1:395378570353:web:24e79d456148a69d3c97d0",
-  measurementId: "G-H4R6RN498J"
+
 };
 
 @NgModule({
